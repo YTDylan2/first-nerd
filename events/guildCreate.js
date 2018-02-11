f464eb55ceb3bd79c77866d543a85d2d3012633b
technoturret <36348775+technoturret@users.noreply.github.com>
2018-02-11 16:12:06 -0500

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,4 +3,5 @@
 module.exports = (client, guild) => {
   // We need to add this guild to our settings!
   client.settings.set(guild.id, client.config.defaultSettings);
+  client.user.setActivity("Techno World. Use a!help. Serving " + client.users.size + " in " + client.guilds.size + " servers." , "Hi.", "https://www.roblox.com/My/Groups.aspx?gid=3643510", "PLAYING")
 };
