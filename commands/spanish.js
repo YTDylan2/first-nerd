5fbc108c37a83bf109dfbe531dcf2c55ad2edafd
technoturret <36348775+technoturret@users.noreply.github.com>
2018-02-11 16:18:38 -0500

    Update spanish.js

--- commands/spanish.js
+++ commands/spanish.js
@@ -2,9 +2,9 @@
 
 exports.run = (client, message, args, level) => {
     var translate = require('translate')
-     let text = message.content.slice("a!spanish".length)
+    let text = message.content.slice("a!spanish".length)
     let transKey = 'trnsl.1.1.20180211T004002Z.a79c5201f3d273a0.281b2e5318c73930291c2ab1760f4f83c731c575'
-    if (text === undefined) {
+    if (!text) {
         message.channel.send("Blank is blanco in spanish.")
         return;
     }
