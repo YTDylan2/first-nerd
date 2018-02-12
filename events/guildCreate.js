511b967691ead6ff9713c5f9ecf0f2772c5ec66f
technoturret <36348775+technoturret@users.noreply.github.com>
2018-02-11 19:24:06 -0500

    Delete guildCreate.js

--- events/guildCreate.js
+++
@@ -1,7 +0,0 @@
-// This event executes when a new guild (server) is joined.
-
-module.exports = (client, guild) => {
-  // We need to add this guild to our settings!
-  client.settings.set(guild.id, client.config.defaultSettings);
-  client.user.setActivity("Techno World. Use a!help. Serving " + client.users.size + " in " + client.guilds.size + " servers." , "Hi.", "https://www.roblox.com/My/Groups.aspx?gid=3643510", "PLAYING")
-};
