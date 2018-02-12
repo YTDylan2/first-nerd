8ce45b215e032b6bd118a946492135bbcefd04c3
technoturret <36348775+technoturret@users.noreply.github.com>
2018-02-11 19:24:24 -0500

    Add files via upload

---
+++ events/guildDelete.js
@@ -0,0 +1,9 @@
+// This event executes when a new guild (server) is left.
+
+module.exports = (client, guild) => {
+  // Well they're gone. Let's remove them from the settings!
+    client.settings.delete(guild.id);
+    let channel = client.channels.find(387789444222287881)
+    channel.send("I have left a guild! **(" + guild.name + ")**")
+client.user.setActivity("Techno World. Use a!help. Serving " + client.users.size + " users in " + client.guilds.size + " servers." , "Hi.", "https://www.roblox.com/My/Groups.aspx?gid=3643510", "PLAYING")
+};
