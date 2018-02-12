383d3b878178ccc6890e8a72c3f48ac8b3a31d73
technoturret <36348775+technoturret@users.noreply.github.com>
2018-02-11 19:24:11 -0500

    Delete guildDelete.js

--- events/guildDelete.js
+++
@@ -1,7 +0,0 @@
-// This event executes when a new guild (server) is left.
-
-module.exports = (client, guild) => {
-  // Well they're gone. Let's remove them from the settings!
-  client.settings.delete(guild.id);
-  client.user.setActivity("Techno World. Use a!help. Serving " + client.users.size + " in " + client.guilds.size + " servers." , "Hi.", "https://www.roblox.com/My/Groups.aspx?gid=3643510", "PLAYING")
-};
