daae85ed68c7a2d1c01fb97e47106ed136e7c94d
technoturret <36348775+technoturret@users.noreply.github.com>
2018-02-12 21:12:23 -0500

    Add files via upload

---
+++ commands/updates.js
@@ -0,0 +1,38 @@
+/*
+updates!!
+*/
+
+exports.run = (client, message, args, level) => {
+    var discord = require('discord.js')
+    var request = require('request')
+    var embed = new discord.RichEmbed();
+    let currentLog = 'v1.12'
+    https('https://api.roblox.com/Marketplace/ProductInfo?assetId=1416632167', { json: true }, (err, res, body) => {
+        if (err) {
+            message.channel.send("Error retreiving update data.")
+            return console.log(err);
+        }
+        embed.addField(`**Updates for ${currentLog}**`, https.Description)
+        embed.setTitle("**Updates**")
+        embed.setDescription("These are the updates for " + currentLog + ".")
+        embed.setColor(6605055)
+        embed.setAuthor("Aureum Studios | techno turret", 'https://imgur.com/WcypWFd')
+        embed.setFooter("Provided by Aureum Studios")
+        embed.setTimestamp()
+    });
+    message.channel.send({ embed })
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["updatelog"],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "updates",
+  category: "System",
+  description: "Displays any new updates available!",
+  usage: "updates"
+};
