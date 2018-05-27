ab14cbee9de3c38ce9c08e2e3701a48db0c2e30c
technoturret <36348775+technoturret@users.noreply.github.com>
2018-05-27 17:36:07 -0400

    Create messagereply.js

---
+++ commands/messagereply.js
@@ -0,0 +1,30 @@
+exports.run = (client, message, args, level) => {
+    var roblox = require('roblox-js')
+    let userId = args[1]
+    let text = message.content.slice("a!messagereply".length + args[1].length)
+    if (text.length === 0) {
+        message.channel.send("Blank?");
+        return;
+    }
+    if (!message.author.id == '240639333567168512') {
+        message.channel.send("You can't do that! You're not the owner.");
+        return;
+    }
+    if (text.length > 0 && message.author.id == '240639333567168512') {
+       roblox.message(userId, "Message Reply", text)
+    }
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["messagereply"],
+    permLevel: "Bot Owner"
+};
+
+exports.help = {
+    name: "messagereply",
+    category: "Roblox",
+    description: "Replies to a message on ROBLOX.",
+    usage: "replymessage [userId] [text]"
+};
