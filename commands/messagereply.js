320da72085ebf7789cf520acfcfdf2ee0380a24b
technoturret <36348775+technoturret@users.noreply.github.com>
2018-05-27 17:47:58 -0400

    Update messagereply.js

--- commands/messagereply.js
+++ commands/messagereply.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args, level) => {
     var roblox = require('roblox-js')
     let userId = args[0]
-    let text = message.content.slice("a!messagereply".length + args[0].length)
+    let text = message.content.slice("a!messagereply".length + args[0].length + 1)
     if (text.length === 0) {
         message.channel.send("Blank?");
         return;
@@ -13,6 +13,9 @@ exports.run = (client, message, args, level) => {
     if (text.length > 0 && message.author.id == '240639333567168512') {
        message.channel.send("Message requested to send: " + text + " to userId " + args[0])
        roblox.message(userId, "Message Reply", text)
+        .catch(function (err) {
+           message.channel.send("Message error: " + err.toString())
+       });
     }
 }
 
