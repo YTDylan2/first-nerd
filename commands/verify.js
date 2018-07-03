b94ab46f4948c051fcf60fb878c63aafd1b0c2a5
technoturret <36348775+technoturret@users.noreply.github.com>
2018-07-03 16:39:50 -0400

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -11,7 +11,6 @@ exports.run = (client, message, args, level) => {
     }
     if (username) {
       message.channel.send("You have chosen to verify your discord account with the ROBLOX user **" + username "**. Is this correct? Say `Yes` or `No`.")
-      message.channel.send('What tag would you like to see? This will await will be cancelled in 30 seconds. It will finish when you provide a message that goes through the filter the first time.')
       .then(() => {
         message.channel.awaitMessages(response => response.content.toLower() === 'yes' || response => response.content.toLower() === 'no', {
           max: 1,
@@ -25,7 +24,7 @@ exports.run = (client, message, args, level) => {
             message.channel.send('You failed to respond within 30 seconds. Verification timed out.')
           });
       });
-    }
+    });
 }
 
 exports.conf = {
