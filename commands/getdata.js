248e14eba4c05698730750d96e5f2ea841d9c65a
technoturret <36348775+technoturret@users.noreply.github.com>
2018-07-07 18:24:52 -0400

    Update getdata.js

--- commands/getdata.js
+++ commands/getdata.js
@@ -1,21 +1,100 @@
+// case legends exclusive
+
 exports.run = (client, message, args, level) => {
-    let enmap = require('enmap')
-    let enmapLevel = require('enmap-level')
+    let data = client.caseLegendsPlayerData
     let key = args[0]
-    var gotValue = client.collection.get(key)
-    message.channel.send(gotValue || "No data for this key.")
+    let found = false;
+    if (key === undefined) {
+        message.reply("A key is needed to get stats. `Say " + client.config.prefix + "check [user OR userId]`")
+        return;
+    }
+    let keyParse = parseInt(key)
+    if (isNan(keyParse)) {
+        // this is a name.
+        for (x in data) {
+            let dataTable = data[x]
+            if (dataTable.name.toLowerCase() == key.toLowerCase()) {
+                found = true
+                var RAP = dataTable.RAP
+                var itemCount = dataTable.Itemcount
+                var highItem = dataTable.HighestItem
+                var bux = dataTable.Bux
+                var rebirths = dataTable.Rebirths
+                var rubies = dataTable.Rubies
+                var CPO = dataTable.Cases
+                var uniqueItems = dataTable.UniqueItemCount
+                const embed = new Discord.RichEmbed()
+                .addField("Player's RAP", RAP)
+                .addField("Player's Case-Bux", bux)
+                .addField("Player's Rubies", rubies)
+                .addField("Player's Rebirths", rebirths)
+                .addField("Player's Cases Per Open", CPO)
+                .addField("Player's Item Count", itemCount)
+                .addField("Item with Highest Amount", highItem)
+                .addField("Unique Amount of Items In Inventory", uniqueItems)
+                .setTitle(`Stats for ${dataTable.name}`)
+                .setDescription(`The data retrieved for ${dataTable.name}. Updated every 30 seconds if the user ingame. Returns their last save if not ingame.`)
+                .setColor(16776960)
+                // .setImage('https://i.imgur.com/zwMrlQT.png')
+                .setThumbnail('https://www.roblox.com/bust-thumbnail/image?userId='+ dataTable.userId + '&width=420&height=420&format=png')
+                .setAuthor("WaterIsIceSoup | water", 'https://i.imgur.com/qMyW7KX.png')
+                .setFooter("Provided by WaterIsIceSoup", 'https://i.imgur.com/qMyW7KX.png')
+                .setTimestamp()
+                message.channel.send({embed})
+                break;
+            }
+        }
+    else {
+        // this is a userId.
+        for (x in data) {
+            let dataTable = data[x]
+            if (dataTable.userId.toLowerCase() == key.toLowerCase()) {
+                found = true
+                var RAP = dataTable.RAP
+                var itemCount = dataTable.Itemcount
+                var highItem = dataTable.HighestItem
+                var bux = dataTable.Bux
+                var rebirths = dataTable.Rebirths
+                var rubies = dataTable.Rubies
+                var CPO = dataTable.Cases
+                var uniqueItems = dataTable.UniqueItemCount
+                const embed = new Discord.RichEmbed()
+                .addField("Player's RAP", RAP)
+                .addField("Player's Case-Bux", bux)
+                .addField("Player's Rubies", rubies)
+                .addField("Player's Rebirths", rebirths)
+                .addField("Player's Cases Per Open", CPO)
+                .addField("Player's Item Count", itemCount)
+                .addField("Item with Highest Amount", highItem)
+                .addField("Unique Amount of Items In Inventory", uniqueItems)
+                .setTitle(`Stats for ${dataTable.name}`)
+                .setDescription(`The data retrieved for ${dataTable.name}. Updated every 30 seconds if the user ingame. Returns their last save if not ingame.`)
+                .setColor(16776960)
+                // .setImage('https://i.imgur.com/zwMrlQT.png')
+                .setThumbnail('https://www.roblox.com/bust-thumbnail/image?userId='+ dataTable.userId + '&width=420&height=420&format=png')
+                .setAuthor("WaterIsIceSoup | water", 'https://i.imgur.com/qMyW7KX.png')
+                .setFooter("Provided by WaterIsIceSoup", 'https://i.imgur.com/qMyW7KX.png')
+                .setTimestamp()
+                message.channel.send({embed})
+                break;
+            }
+        }
+    }
+    if (found == false) {
+        message.reply("The user has not joined after this update was applied, or the user is invalid.")
+    }
 }
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
     aliases: [""],
-    permLevel: "Bot Owner"
+    permLevel: "Administrator"
 };
 
 exports.help = {
-    name: "get",
-    category: "Utility",
+    name: "check",
+    category: "Roblox",
     description: "For data persistence testing.",
-    usage: "get [arg] [val]"
+    usage: "check [user OR userId]"
 };
