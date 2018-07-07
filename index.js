9f15aa5da20b715b714c0e0790d4a978bce06f21
technoturret <36348775+technoturret@users.noreply.github.com>
2018-07-07 18:36:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ client.aliases = new Enmap();
 // and makes things extremely easy for this purpose.
 client.settings = new Enmap({provider: new EnmapLevel({name: "settings"})});
 
-client.collection = new Enmap({provider: new EnmapLevel(name: "enmapTest"})});
+client.collection = new Enmap({provider: new EnmapLevel({name: "enmapTest"})});
 
 // We're doing real fancy node 8 async/await stuff here, and to do that
 // we need to wrap stuff in an anonymous function. It's annoying but it works.
