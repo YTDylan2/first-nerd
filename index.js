a4234de8a5f015c8ca19804407a4f1a5d079d6e1
technoturret <36348775+technoturret@users.noreply.github.com>
2018-07-07 18:45:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -308,7 +308,7 @@ const init = async () => {
         }
         
         if (idsLogged[data.userId]) {
-            for (x in client.caseLegendsPlayerData)
+            for (x in client.caseLegendsPlayerData) {
                 var data = client.caseLegendsPlayerData[x]
                 if (findUserIdMatch(data.userId, params.data)) {
                     data = params  
