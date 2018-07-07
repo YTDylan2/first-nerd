fc8c4b55dad9efc8ed4d03a71f4bb6dde78d83f8
technoturret <36348775+technoturret@users.noreply.github.com>
2018-07-07 14:05:21 -0400

    Update reboot.js

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,8 +1,9 @@
 exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
-  await message.reply("Bot is shutting down.");
+  await message.reply("bot is shutting down.");
   client.commands.forEach( async cmd => {
     await client.unloadCommand(cmd);
   });
+  await client.collection.db.close();
   process.exit(1);
 };
 
