35f1d5ac7395ef44238991c105584797fd42ecc7
technoturret <36348775+technoturret@users.noreply.github.com>
2018-07-08 20:46:32 -0400

    Update groupBan.js

--- commands/groupBan.js
+++ commands/groupBan.js
@@ -1,10 +1,10 @@
 exports.run = (client, message, args, level) => {
-    let banned = require("./data/groupBan.js")
+    let banned = require("../data/groupBan.js")
     const fs = require("fs")
     let toBan = args[0]
     if (!isNaN(parseInt(toBan))) {
       banned[toBan] = true;
-      fs.writeFile("./data/groupBan.js", JSON.stringify(banned), (err) => console.error);
+      fs.writeFile("../data/groupBan.js", JSON.stringify(banned), (err) => console.error);
       return;
     }
      if (isNaN(parseInt(toBan))) {
