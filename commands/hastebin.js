a2c808a1eeb4a0049068c8b0e022ed3ea6a3f983
technoturret <36348775+technoturret@users.noreply.github.com>
2018-07-08 21:23:58 -0400

    Update hastebin.js

--- commands/hastebin.js
+++ commands/hastebin.js
@@ -2,8 +2,8 @@ exports.run = (client, message, args, level) => {
     var roblox = require('roblox-js')
     var num = 0
     let text = args.join(" ")
-    var post = require("snekfetch");
-    var body = await post("https://www.hastebin.com/documents").send(text);
+    const { post } = require("snekfetch");
+    const { body } = await post("https://www.hastebin.com/documents").send(text);
     message.channel.send(`Sent to https://www.hastebin.com/${body.key}.txt`)
 
     
