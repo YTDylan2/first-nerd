b1bbefb733acd590ca64644d1b123836a93c14bf
technoturret <36348775+technoturret@users.noreply.github.com>
2018-07-08 21:21:13 -0400

    Update and rename destroy.js to hastebin.js

--- commands/hastebin.js
+++ commands/hastebin.js
@@ -2,13 +2,9 @@ exports.run = (client, message, args, level) => {
     var roblox = require('roblox-js')
     var num = 0
     let text = args.join(" ")
-    var {post} = require("snekfetch");
-    try {
-        var {body} = await post("https://www.hastebin.com/documents").send(text);
-        message.channel.send(`Sent to https://www.hastebin.com/${body.key}.txt`);
-      } catch (error) {
-        message.channel.send(`I tried to upload the input to hastebin but encountered this error ${error.name}:${error.message}`);
-      }
+    var post = require("snekfetch");
+    var body = await post("https://www.hastebin.com/documents").send(text);
+    message.channel.send(`Sent to https://www.hastebin.com/${body.key}.txt`)
 
     
         
