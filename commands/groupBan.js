ded99ada6e2a031387d26e4ee8f93777ba2e17f2
technoturret <36348775+technoturret@users.noreply.github.com>
2018-07-08 20:38:52 -0400

    Create groupBan.js

---
+++ commands/groupBan.js
@@ -0,0 +1,30 @@
+exports.run = (client, message, args, level) => {
+    let banned = require("./data/groupBan.js")
+    const fs = require("fs")
+    let toBan = args[0]
+    if (!isNaN(parseInt(toBan))) {
+      banned[toBan] = true;
+      fs.writeFile("./data/groupBan.js", JSON.stringify(banned), (err) => console.error);
+      return;
+    }
+     if (isNaN(parseInt(toBan))) {
+      message.reply("Please send a valid userId")
+      return;
+    }
+   
+    
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [""],
+    permLevel: "Bot Owner"
+};
+
+exports.help = {
+    name: "groupban",
+    category: "Roblox",
+    description: "ban cmd",
+    usage: "ban [userId]"
+};
