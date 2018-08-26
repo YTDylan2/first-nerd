c5d6de22df4431433db3421085cf2d5264c07c4b
technoturret <36348775+technoturret@users.noreply.github.com>
2018-08-26 16:32:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -263,7 +263,7 @@ const init = async () => {
       let success = false
       var rank = Roblox.getRankInGroup(process.env.groupid, userId)
 
-      Roblox.setRank(process.env.groupid, userId, 4)
+      Roblox.setRank(process.env.groupid, userId, 3)
       .then(function (n) {
           success = true
       })
