ef38c390304d611ffcab47833c312d613a9e2786
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-06 18:55:33 -0400

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -3,7 +3,7 @@
 exports.run = (client, message, args, level) => {
     let username = args[0]
     var discord = require('discord.js')
-    var enmap = require('enmap')
+    var redis = require('redis')
     if (!username || username == undefined) {
        message.channel.send("You need to send a valid username!")
        return;
@@ -17,12 +17,25 @@ exports.run = (client, message, args, level) => {
             errors: ['time'],
         })
         .then((collected) => {
-          message.channel.send("Your choice was " + collected.first().content)
+          if (response.content.match('yes') {
+            
+          } else {
+            message.channel.send("Verification request cancelled.")
+          }
+          message.channel.send("Please add the following words to your personal bio/info page:\n\n```Waterblob Verification```\nReply 'done' when you have done so!") 
+            message.channel.awaitMessages(response => response.author.id == message.author.id && (response.content.toLowerCase().match('done'), {
+            max: 1,
+            time: 120000,
+            errors: ['time'],
+        }).then((collected) => {
+           message.channel.send("this is where we check the data boy")
+        }).catch(() => {
+          message.channel.send('You failed to complete the prompt within 2 minutes! Your verification timed out. :(')
         })
-        .catch(() => {
-          message.channel.send('You failed to respond within 1 minute! Your verification timed out. :(')
-        });
-     });
+                                                                 
+     }).catch(() => {
+      message.channel.send('You failed to respond within 1 minute! Your verification timed out. :(')
+    });
    };
 }
 
