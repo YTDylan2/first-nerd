2a7886717899cf9c51f08b4f62f31be3086653d1
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-08 15:13:31 -0400

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -11,68 +11,67 @@ exports.run = (client, message, args, level) => {
        message.channel.send("You need to send a valid username!")
        return;
     }
-   let id = roblox.getIdFromUsername(username)
-    .catch(() => { 
+   roblox.getIdFromUsername(username)
+    .then(id => { 
+        // already verified
+        client.redisClient.get(message.author.id, function(err, reply) {
+            if (reply != null) {
+                 message.channel.send(`You've already been verified to **${reply}**!`)
+                 return;
+            }
+        })
+
+        // trying to verify to another user
+        var storedData = client.redisClient.get(id.toString(), function(err, reply) {
+             if (reply != null) {
+                 var user = client.users.get(reply)
+                 if (user) { 
+                    message.channel.send("That roblox account has already been verified to **" + user.username + "**!")
+                    return;
+                 }             
+             }
+        })
+
+        if (id != null) {
+          message.channel.send("You have chosen to verify your discord account with the ROBLOX user **" + username + "**. Is this correct? Say `Yes` or `No`. (is this you?) - http://www.https://www.roblox.com/users/" + id +"/profile")
+            message.channel.awaitMessages(response => response.author.id == message.author.id && (response.content.toLowerCase().match('yes') || response.content.toLowerCase().match('no')), {
+                max: 1,
+                time: 60000,
+                errors: ['time'],
+            }).then(collected => {
+                  if (collected.first().content.match('yes')) {
+                     message.channel.send("Please place the words:\n\n**Waterblob Verification**\n\ninto your STATUS (What are you up to?) section. Reply 'finished' when done so.")
+                     message.channel.awaitMessages(response => response.author.id == message.author.id && (response.content.toLowerCase().match('finished')), {
+                        max: 1,
+                        time: 120000,
+                        errors: ['time'],
+                    }).then(collected => {
+                         roblox.getStatus(id)
+                         .then(status => {
+                             if (status.lower().match('waterblob verification')) {
+                                 client.redisClient.set(message.author.id, id.toString())
+                                 client.redisClient.set(id.toString(), message.author.id())
+                                 message.channel.send('✅ Successfully verified!')
+                             }
+                             if (!status.lower().match('waterblob verification')) {
+                                 message.channel.send('❎ oof... could not verify you. Did you enter the phrase into your status correctly?')
+                             }
+                         })
+                     }).catch(() => {
+                         message.channel.send("oof, your verification timed out!")
+                     })
+                  }
+                  if (collected.first().content.match('no')) { 
+                     message.channel.send("Verification cancelled.")
+                  }
+            }).catch(() => {
+              message.channel.send('You failed to respond within 1 minute! Your verification timed out. :(')
+            })                                                                                 
+       };
+    }).catch(() => { 
         message.channel.send("You need to send a valid username! Case sensitive.") 
         return; 
     }) 
-    // not valid user
-    
-    // already verified
-    client.redisClient.get(message.author.id, function(err, reply) {
-        if (reply != null) {
-             message.channel.send(`You've already been verified to **${reply}**!`)
-             return;
-        }
-    })
-    
-    // trying to verify to another user
-    var storedData = client.redisClient.get(id.toString(), function(err, reply) {
-         if (reply != null) {
-             var user = client.users.get(reply)
-             if (user) { 
-                message.channel.send("That roblox account has already been verified to **" + user.username + "**!")
-                return;
-             }             
-         }
-    })
-    
-    if (id != null) {
-      message.channel.send("You have chosen to verify your discord account with the ROBLOX user **" + username + "**. Is this correct? Say `Yes` or `No`. (is this you?) - http://www.https://www.roblox.com/users/" + id +"/profile")
-        message.channel.awaitMessages(response => response.author.id == message.author.id && (response.content.toLowerCase().match('yes') || response.content.toLowerCase().match('no')), {
-            max: 1,
-            time: 60000,
-            errors: ['time'],
-        }).then(collected => {
-              if (collected.first().content.match('yes')) {
-                 message.channel.send("Please place the words:\n\n**Waterblob Verification**\n\ninto your STATUS (What are you up to?) section. Reply 'finished' when done so.")
-                 message.channel.awaitMessages(response => response.author.id == message.author.id && (response.content.toLowerCase().match('finished')), {
-                    max: 1,
-                    time: 120000,
-                    errors: ['time'],
-                }).then(collected => {
-                     roblox.getStatus(id)
-                     .then(status => {
-                         if (status.lower().match('waterblob verification')) {
-                             client.redisClient.set(message.author.id, id.toString())
-                             client.redisClient.set(id.toString(), message.author.id())
-                             message.channel.send('✅ Successfully verified!')
-                         }
-                         if (!status.lower().match('waterblob verification')) {
-                             message.channel.send('❎ oof... could not verify you. Did you enter the phrase into your status correctly?')
-                         }
-                     })
-                 }).catch(() => {
-                     message.channel.send("oof, your verification timed out!")
-                 })
-              }
-              if (collected.first().content.match('no')) { 
-                 message.channel.send("Verification cancelled.")
-              }
-        }).catch(() => {
-          message.channel.send('You failed to respond within 1 minute! Your verification timed out. :(')
-        })                                                                                 
-   };
 }
 
 exports.conf = {
