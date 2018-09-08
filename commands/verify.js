8aa65309b9effdeff4b628fd9e7580f49a41d447
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-08 15:14:34 -0400

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -33,7 +33,7 @@ exports.run = (client, message, args, level) => {
         })
 
         if (id != null) {
-          message.channel.send("You have chosen to verify your discord account with the ROBLOX user **" + username + "**. Is this correct? Say `Yes` or `No`. (is this you?) - http://www.https://www.roblox.com/users/" + id +"/profile")
+          message.channel.send("You have chosen to verify your discord account with the ROBLOX user **" + username + "**. Is this correct? Say `Yes` or `No`. (is this you?) - https://www.roblox.com/users/" + id +"/profile")
             message.channel.awaitMessages(response => response.author.id == message.author.id && (response.content.toLowerCase().match('yes') || response.content.toLowerCase().match('no')), {
                 max: 1,
                 time: 60000,
