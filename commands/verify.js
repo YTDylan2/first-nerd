a432ac04de38cd63a98d47e9aa825449104d9a1b
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-08 15:51:59 -0400

    logic checks

--- commands/verify.js
+++ commands/verify.js
@@ -40,41 +40,42 @@ exports.run = (client, message, args, level) => {
          }
          if (reply == null && proceed) {
              if (id != null && proceed) {
-            message.channel.send("You have chosen to verify your discord account with the ROBLOX user **" + username + "**. Is this correct? Say `Yes` or `No`. (is this you?) - https://www.roblox.com/users/" + id +"/profile")
-            message.channel.awaitMessages(response => response.author.id == message.author.id && (response.content.toLowerCase().match('yes') || response.content.toLowerCase().match('no')), {
-                max: 1,
-                time: 60000,
-                errors: ['time'],
-            }).then(collected => {
-                  if (collected.first().content.match('yes')) {
-                     message.channel.send("Please place the words:\n\n**Waterblob Verification**\n\ninto your STATUS (What are you up to?) section. Reply 'finished' when done so.")
-                     message.channel.awaitMessages(response => response.author.id == message.author.id && (response.content.toLowerCase().match('finished')), {
-                        max: 1,
-                        time: 120000,
-                        errors: ['time'],
-                    }).then(collected => {
-                         roblox.getStatus(id)
-                         .then(status => {
-                             if (status.toLowerCase().match('waterblob verification')) {
-                                 client.redisClient.set(message.author.id, id.toString())
-                                 client.redisClient.set(id.toString(), message.author.id)
-                                 message.channel.send('✅ Successfully verified!')
-                             }
-                             if (!status.toLowerCase().match('waterblob verification')) {
-                                 message.channel.send('❎ oof... could not verify you. Did you enter the phrase into your status correctly?')
-                             }
+                message.channel.send("You have chosen to verify your discord account with the ROBLOX user **" + username + "**. Is this correct? Say `Yes` or `No`. (is this you?) - https://www.roblox.com/users/" + id +"/profile")
+                message.channel.awaitMessages(response => response.author.id == message.author.id && (response.content.toLowerCase().match('yes') || response.content.toLowerCase().match('no')), {
+                    max: 1,
+                    time: 60000,
+                    errors: ['time'],
+                }).then(collected => {
+                      if (collected.first().content.match('yes')) {
+                         message.channel.send("Please place the words:\n\n**Waterblob Verification**\n\ninto your STATUS (What are you up to?) section. Reply 'finished' when done so.")
+                         message.channel.awaitMessages(response => response.author.id == message.author.id && (response.content.toLowerCase().match('finished')), {
+                            max: 1,
+                            time: 120000,
+                            errors: ['time'],
+                        }).then(collected => {
+                             roblox.getStatus(id)
+                             .then(status => {
+                                 if (status.toLowerCase().match('waterblob verification')) {
+                                     client.redisClient.set(message.author.id, id.toString())
+                                     client.redisClient.set(id.toString(), message.author.id)
+                                     message.channel.send('✅ Successfully verified!')
+                                 }
+                                 if (!status.toLowerCase().match('waterblob verification')) {
+                                     message.channel.send('❎ oof... could not verify you. Did you enter the phrase into your status correctly?')
+                                 }
+                             })
+                         }).catch(() => {
+                             message.channel.send("oof, your verification timed out!")
                          })
-                     }).catch(() => {
-                         message.channel.send("oof, your verification timed out!")
-                     })
-                  }
-                  if (collected.first().content.match('no')) { 
-                     message.channel.send("Verification cancelled.")
-                  }
-            }).catch(() => {
-              message.channel.send('You failed to respond within 1 minute! Your verification timed out. :(')
-            })                                                                                 
-       };  
+                      }
+                      if (collected.first().content.match('no')) { 
+                         message.channel.send("Verification cancelled.")
+                      }
+                }).catch(() => {
+                  message.channel.send('You failed to respond within 1 minute! Your verification timed out. :(')
+                })                                                                                 
+           };
+         }
     })  
 }
 
