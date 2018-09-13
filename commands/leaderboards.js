4707d196ed551ac4da03e7a6af777c292a80204e
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-13 19:34:48 -0400

    ugh im dumb, forgot redis needs cb

--- commands/leaderboards.js
+++ commands/leaderboards.js
@@ -4,37 +4,38 @@ exports.run = (client, message, args, level) => {
    var sorted = [];
    var discord = require('discord.js')
    
-   var data = client.redisClient.get("Global Coins")
-    if (data) {
-      let parsed = JSON.parse(data)
-      for (x in parsed) {
-         sorted.push([x + '', parsed[x]])
-      }
-      sorted.sort(function(a, b) {
-         return a[1] - b[1]
-      })
+   client.redisClient.get("Global Coins", function(err, data) {
+       if (data) {
+         let parsed = JSON.parse(data)
+         for (x in parsed) {
+            sorted.push([x + '', parsed[x]])
+         }
+         sorted.sort(function(a, b) {
+            return a[1] - b[1]
+         })
 
-      let display = ""
-      for (i = 0; i < 25; i++) {
-         var userTable = sorted[i]
-         console.log("user table is " + userTable)
-         if (userTable) {
-            let member = client.users.get(userTable[0])
-            console.log(member + " (member)")
-            if (member) {
-               display = display + (i + ". **" + member.tag + "** - **" + userTable[1] + "** coins\n")
+         let display = ""
+         for (i = 0; i < 25; i++) {
+            var userTable = sorted[i]
+            console.log("user table is " + userTable)
+            if (userTable) {
+               let member = client.users.get(userTable[0])
+               console.log(member + " (member)")
+               if (member) {
+                  display = display + (i + ". **" + member.tag + "** - **" + userTable[1] + "** coins\n")
+               }
             }
          }
-      }
-      const embed = new discord.RichEmbed()
-      .setTitle("Top 25 Global Users")
-      .addField("List", display + "\nfiller")
-      .setColor(6579455)
+         const embed = new discord.RichEmbed()
+         .setTitle("Top 25 Global Users")
+         .addField("List", display + "\nfiller")
+         .setColor(6579455)
 
-     message.channel.send({embed})
-    } else {
-       message.channel.send("nobody on the leaderboards yet b")
-    }
+        message.channel.send({embed})
+       } else {
+          message.channel.send("nobody on the leaderboards yet b")
+       }
+   })
 }
 exports.conf = {
     enabled: true,
