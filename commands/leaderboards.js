e581435517ac84fb1c0d8f989dd75f02d0494a60
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-12 20:43:59 -0400

    Update leaderboards.js

--- commands/leaderboards.js
+++ commands/leaderboards.js
@@ -5,35 +5,35 @@ exports.run = (client, message, args, level) => {
    var discord = require('discord.js')
    
    client.redisClient.get("Global Coins", function(err, data) {
-      if (data) {
-        for (x in data) {
-          sorted.push([x, data[x]])
-        }
-        sorted.sort(function(a, b) {
-          return a > b
-        })
-        
-        let display = []
-        for (i = 0; i < 25; i++) {
-          var userTable = sorted[i]
-          if (userTable) {
-            let member = client.users.get(userTable[0])
-            if (member) {
-              display.push(i + ". **" + member.tag + "** - **" userTable[1] + "** coins\n")
+       if (data) {
+         for (x in data) {
+            sorted.push([x, data[x]])
+         }
+         sorted.sort(function(a, b) {
+            return a > b
+         })
+          
+         let display = []
+         for (i = 0; i < 25; i++) {
+            var userTable = sorted[i]
+            if (userTable) {
+               let member = client.users.get(userTable[0])
+               if (member) {
+               display.push(i + ". **" + member.tag + "** - **" userTable[1] + "** coins\n")
+               }
             }
-          }
-        }
-        let str = display.join(" ")
-        const embed = new discord.RichEmbed()
-        .setTitle("Top 25 Global Users")
-        .addField("List", str)
-        .setColor(6579455)
+         }
+         let str = display.join(" ")
+         const embed = new discord.RichEmbed()
+         .setTitle("Top 25 Global Users")
+         .addField("List", str)
+         .setColor(6579455)
         
         message.channel.send({embed})
-      } else {
-        message.channel.send("nobody on the leaderboards yet")
-      }
-   })
+       } else {
+          message.channel.send("nobody on the leaderboards yet")
+       }
+   })                      
 }
 exports.conf = {
     enabled: true,
