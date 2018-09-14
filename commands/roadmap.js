4023900bba8459cab8f0160b99b66eda7344981c
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-14 16:22:29 -0400

    Create roadmap.js

---
+++ commands/roadmap.js
@@ -0,0 +1,24 @@
+// ban user
+
+exports.run = (client, message, args, level) => {
+   client.redisClient.get("Roadmap", function(err, roadmapData) {
+      if (roadmapData) {
+        console.log(level(
+      }
+    }
+    
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [""],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "roadmap",
+    category: "System",
+    description: "Shows any planned updates.",
+    usage: "roadmap"
+};
