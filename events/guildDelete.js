2e535bdfd4b46d6b01104e079fd3d506649bf20e
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-14 21:52:06 -0400

    change msg

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -3,5 +3,5 @@
 module.exports = (client, guild) => {
   // Well they're gone. Let's remove them from the settings!
     client.settings.delete(guild.id);
-client.user.setActivity("Techno World. Use a!help. Serving " + client.users.size + " users in " + client.guilds.size + " servers." , "Hi.", "https://www.roblox.com/My/Groups.aspx?gid=3643510", "PLAYING")
+//client.user.setActivity("Techno World. Use a!help. Serving " + client.users.size + " users in " + client.guilds.size + " servers." , "Hi.", "https://www.roblox.com/My/Groups.aspx?gid=3643510", "PLAYING")
 };
