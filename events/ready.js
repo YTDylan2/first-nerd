1ac4aec5e068097bdd2eaeb33463c0b087e49cf8
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-15 16:20:53 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,10 +4,11 @@ module.exports = async client => {
   // for all of them to be loaded.
   var ordinal = require('ordinal-js')
   await client.wait(1000);
+  let buildVer = parseInt(process.env.HEROKU_RELEASE_VERSION)
 
   // Both `wait` and `client.log` are in `./modules/functions`.
   client.logger.log(`[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
-  client.user.setActivity(`in the ${ordinal.toOrdinal(parseInt(process.env.HEROKU_RELEASE_VERSION))} timeline. Use a!help. ` + client.guilds.size + " guilds." , "Hi.", "https://www.roblox.com/My/Groups.aspx?gid=3643510", "PLAYING")
+  client.user.setActivity(`in the ${ordinal.toOrdinal(buildVer)} timeline. Use a!help. ` + client.guilds.size + " guilds." , "Hi.", "https://www.roblox.com/My/Groups.aspx?gid=3643510", "PLAYING")
   // We check for any guilds added while the bot was offline, if any were, they get a default configuration.
   client.guilds.filter(g => !client.settings.has(g.id)).forEach(g => client.settings.set(g.id, client.config.defaultSettings));
 };
