f6621927695b5dd1f0323cfa5c72a85617fbe15d
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-15 16:49:17 -0400

    updated to use redis

--- events/ready.js
+++ events/ready.js
@@ -14,5 +14,5 @@ module.exports = async client => {
   client.logger.log(`[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
   client.user.setActivity(`in the ${ordinal.toOrdinal(numb)} timeline. Use a!help. ` + client.guilds.size + " guilds." , "Hi.", "https://www.roblox.com/My/Groups.aspx?gid=3643510", "PLAYING")
   // We check for any guilds added while the bot was offline, if any were, they get a default configuration.
-  client.guilds.filter(g => !client.settings.has(g.id)).forEach(g => client.settings.set(g.id, client.config.defaultSettings));
+  client.guilds.filter(g => !client.redisCient.get(g.id)).forEach(g => client.redisClient.set(g.id, client.config.defaultSettings));
 };
