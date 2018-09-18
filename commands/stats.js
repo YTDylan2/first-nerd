d35d86e2af9170234aebc77d6e925ba8c9f8b831
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-18 19:24:17 -0400

    updated to look fancier

--- commands/stats.js
+++ commands/stats.js
@@ -4,17 +4,25 @@ require("moment-duration-format");
 
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-  message.channel.send(`= STATISTICS =
-• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Uptime     :: ${duration}
-• Users      :: ${client.users.size.toLocaleString()}
-• Servers    :: ${client.guilds.size.toLocaleString()}
-• Channels   :: ${client.channels.size.toLocaleString()}
-• Discord.js :: v${version}
-• Node       :: ${process.version}
-• Began running at :: ${process.env.HEROKU_RELEASE_CREATED_AT}
-• Build Version :: ${process.env.HEROKU_RELEASE_VERSION}`, {code: "asciidoc"});
+  var discord = require("discord.js")
+  
+  const embed = new discord.RichEmbed()
+  .setAuthor("Vanessa", client.user.avatarURL)
+  .setTitle("Bot Statistics")
+  .setDescription("Some collected statistics for Vanessa!")
+  .addField("Memory Usage", (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2))
+  .addField("Uptime", duration)
+  .addField("Users", client.users.size.toLocaleString())
+  .addField("Servers", client.guilds.size.toLocaleString())
+  .addField("Channels", client.channels.size.toLocaleString())
+  .addField("Discord.js Version", "v" + version)
+  .addField("Node Version", process.version)
+  .addField("Build Version", process.env.HEROKU_RELEASE_VERSION)
+  .setTimestamp()
+  .setColor(process.env.purple)
+  message.channel.send({embed})
 };
+   
 
 exports.conf = {
   enabled: true,
