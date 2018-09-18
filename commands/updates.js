88f452ea956b4d4a280c83e1af05b2fa904f9264
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-18 19:57:14 -0400

    changed around

--- commands/updates.js
+++ commands/updates.js
@@ -6,22 +6,19 @@ exports.run = (client, message, args, level) => {
     var discord = require('discord.js')
     var request = require('request')
     
-    let currentLog = 'v1.12'
+    let currentLog = ''
     let desc = null
     request('https://api.roblox.com/Marketplace/ProductInfo?assetId=1416632167', { json: true }, (err, res, body) => {
         if (err) {
-            message.channel.send("Error retreiving update data.")
+            message.channel.send("Error retreiving update data!")
             return console.log(err);
         }
         const embed = new discord.RichEmbed()
-        .addField(`**Updates for ${currentLog}**`, body.Description)
-        .setTitle("**Updates**")
-        .setDescription("These are the updates for " + currentLog + ".")
-        .setColor(6605055)
-        .setImage('https://i.imgur.com/zwMrlQT.png')
-        .setThumbnail('https://i.imgur.com/WcypWFd.png')
-        .setAuthor("Aureum Studios | techno turret", 'https://i.imgur.com/WcypWFd.png')
-        .setFooter("Provided by Aureum Studios", 'https://i.imgur.com/WcypWFd.png')
+        .setTitle("Updates")
+        .setColor(process.env.purple)
+        .setDescription(body.Description)        
+        .setAuthor("Vanessa", client.user.avatarURL)
+        .setFooter("arky smooth", client.user.avatarURL)
         .setTimestamp()
         message.channel.send({embed})
         
@@ -38,7 +35,7 @@ exports.conf = {
 
 exports.help = {
   name: "updates",
-  category: "System",
+  category: "Info",
   description: "Displays any new updates available!",
   usage: "updates"
 };
