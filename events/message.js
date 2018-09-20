5efe9dfe2772c4d4e99211004fb513edf314787a
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-19 21:11:19 -0400

    fix dms

--- events/message.js
+++ events/message.js
@@ -24,74 +24,76 @@ module.exports = (client, message) => {
   
   // For ease of use in commands and functions, we'll attach the settings
   // to the message object, so `message.settings` is accessible.
-  client.redisClient.get(message.guild.id + "-SETTINGS", function(err, data) {
-      let settings = JSON.parse(data)
-      if (!settings) {
-         settings = client.config.defaultSettings
-      }
-         
-      message.settings = settings;
-      let mentions = message.mentions.members
-      let match =  matchMention(message.content)   
-      if (match) {
-         message.channel.startTyping()
-         client.cleverbot.create(function(bad, session) {
-             client.cleverbot.ask(match, function(err, response) {
-                 message.channel.send(response + ' <@!' + message.author.id + '>')
-                 message.channel.stopTyping()
-             })
-         })
-      }            
+  if (message.guild) {
+     client.redisClient.get(message.guild.id + "-SETTINGS", function(err, data) {
+         let settings = JSON.parse(data)
+         if (!settings) {
+            settings = client.config.defaultSettings
+         }
 
-      // Also good practice to ignore any message that does not start with our prefix,
-      // which is set in the configuration file.
-      if (message.content.indexOf(settings.prefix !== 0) && message.content.indexOf(settings.prefix.toUpperCase()) !== 0) return;
+         message.settings = settings;
+         let mentions = message.mentions.members
+         let match =  matchMention(message.content)   
+         if (match) {
+            message.channel.startTyping()
+            client.cleverbot.create(function(bad, session) {
+                client.cleverbot.ask(match, function(err, response) {
+                    message.channel.send(response + ' <@!' + message.author.id + '>')
+                    message.channel.stopTyping()
+                })
+            })
+         }            
 
-      // Here we separate our "command" name, and our "arguments" for the command.
-      // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-      // command = say
-      // args = ["Is", "this", "the", "real", "life?"]
-      const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
-      const command = args.shift().toLowerCase();
+         // Also good practice to ignore any message that does not start with our prefix,
+         // which is set in the configuration file.
+         if (message.content.indexOf(settings.prefix !== 0) && message.content.indexOf(settings.prefix.toUpperCase()) !== 0) return;
 
-      // Get the user or member's permission level from the elevation
-      const level = client.permlevel(message);
+         // Here we separate our "command" name, and our "arguments" for the command.
+         // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+         // command = say
+         // args = ["Is", "this", "the", "real", "life?"]
+         const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+         const command = args.shift().toLowerCase();
 
-      // Check whether the command, or alias, exist in the collections defined
-      // in app.js.
-      const cmd = client.commands.get(command.toLowerCase()) || client.commands.get(client.aliases.get(command.toLowerCase()));
-      // using this const varName = thing OR otherthign; is a pretty efficient
-      // and clean way to grab one of 2 values!
-      // clever bot
+         // Get the user or member's permission level from the elevation
+         const level = client.permlevel(message);
 
-      if (!cmd) return;
+         // Check whether the command, or alias, exist in the collections defined
+         // in app.js.
+         const cmd = client.commands.get(command.toLowerCase()) || client.commands.get(client.aliases.get(command.toLowerCase()));
+         // using this const varName = thing OR otherthign; is a pretty efficient
+         // and clean way to grab one of 2 values!
+         // clever bot
 
-      // Some commands may not be useable in DMs. This check prevents those commands from running
-      // and return a friendly error message.
-      if (cmd && !message.guild && cmd.conf.guildOnly)
-       return message.channel.send("This command is unavailable via private message. Please run this command in a guild.");
+         if (!cmd) return;
 
-      if (level < client.levelCache[cmd.conf.permLevel]) {
-         return message.channel.send(`You're missing the required permission (${cmd.conf.permLevel}) to use this command!`)
-      }
+         // Some commands may not be useable in DMs. This check prevents those commands from running
+         // and return a friendly error message.
+         if (cmd && !message.guild && cmd.conf.guildOnly)
+          return message.channel.send("This command is unavailable via private message. Please run this command in a guild.");
 
-      // don't run if disabled
-      if (cmd.conf.enabled == false) {
-         return message.channel.send("Looks like this command is disabled.")
-      }
+         if (level < client.levelCache[cmd.conf.permLevel]) {
+            return message.channel.send(`You're missing the required permission (${cmd.conf.permLevel}) to use this command!`)
+         }
 
-      // To simplify message arguments, the author's level is now put on level (not member so it is supported in DMs)
-      // The "level" command module argument will be deprecated in the future.
-      message.author.permLevel = level;
+         // don't run if disabled
+         if (cmd.conf.enabled == false) {
+            return message.channel.send("Looks like this command is disabled.")
+         }
 
-      message.flags = [];
-      while (args[0] && args[0][0] === "-") {
-       message.flags.push(args.shift().slice(1));
-      }
-      // If the command exists, **AND** the user has permission, run it.
-      client.logger.cmd(`[CMD] ${client.config.permLevels.find(l => l.level === level).name} ${message.author.username} (${message.author.id}) ran command ${cmd.help.name}`);
-      client.lastCommand = "a!" + cmd.help.name
-      cmd.run(client, message, args, level);
-  })
+         // To simplify message arguments, the author's level is now put on level (not member so it is supported in DMs)
+         // The "level" command module argument will be deprecated in the future.
+         message.author.permLevel = level;
+
+         message.flags = [];
+         while (args[0] && args[0][0] === "-") {
+          message.flags.push(args.shift().slice(1));
+         }
+         // If the command exists, **AND** the user has permission, run it.
+         client.logger.cmd(`[CMD] ${client.config.permLevels.find(l => l.level === level).name} ${message.author.username} (${message.author.id}) ran command ${cmd.help.name}`);
+         client.lastCommand = settings.prefix + cmd.help.name
+         cmd.run(client, message, args, level);
+     })
+  }
 };
                          
