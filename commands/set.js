ca36144055c087d160be0e218e1bbaca33ac6a53
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-19 20:33:59 -0400

    updated edit cmd

--- commands/set.js
+++ commands/set.js
@@ -14,66 +14,24 @@ const redis = require('redis')
 // OR the same as:
 // const [action, key, ...value] = args;
 exports.run = async (client, message, [action, key, ...value], level) => { // eslint-disable-line no-unused-vars
-
-  // Retrieve current guild settings
-  client.redisClient.get(message.guild.id, function(err, guildData) {
-  
-    // First, if a user does `-set add <key> <new value>`, let's add it
-    if (guildData) {
-      var settings = JSON.parse(guildData)
-      if (action === "add") {
-        if (!key) return message.reply("Please specify a key to add");
-        if (settings[key]) return message.reply("This key already exists in the settings");
-        if (value.length < 1) return message.reply("Please specify a value");
-
-        // `value` being an array, we need to join it first.
-        settings[key] = value.join(" ");
-
-        // One the settings is modified, we write it back to the collection
-        client.redisClient.set(message.guild.id, settings);
-        message.reply(`${key} successfully added with the value of ${value.join(" ")}`);
-      } else
-
-      // Secondly, if a user does `-set edit <key> <new value>`, let's change it
-      if (action === "edit") {
-        if (!key) return message.reply("Please specify a key to edit");
-        if (!settings[key]) return message.reply("This key does not exist in the settings");
-        if (value.length < 1) return message.reply("Please specify a new value");
-
-        settings[key] = value.join(" ");
-
-        client.redisClient.set(message.guild.id, settings);
-        message.reply(`${key} successfully edited to ${value.join(" ")}`);
-      } else
-
-      // Thirdly, if a user does `-set del <key>`, let's ask the user if they're sure...
-      if (action === "del") {
-        if (!key) return message.reply("Please specify a key to delete.");
-        if (!settings[key]) return message.reply("This key does not exist in the settings");
-
-        // Throw the 'are you sure?' text at them.
-        const response = await client.awaitReply(message, `Are you sure you want to permanently delete ${key}? This **CANNOT** be undone.`);
-
-        // If they respond with y or yes, continue.
-        if (["y", "yes"].includes(response)) {
-
-          // We delete the `key` here.
-          delete settings[key];
-          client.redisClient.set(message.guild.id, settings);
-          message.reply(`${key} was successfully deleted.`);
-        } else
-        // If they respond with n or no, we inform them that the action has been cancelled.
-        if (["n","no","cancel"].includes(response)) {
-          message.reply("Action cancelled.");
+  let guildId = message.guild.id
+  client.redisClient.get(guildId + "-SETTINGS", function(err, settings) {
+    if (settings) {
+      let modifiable = JSON.parse(settings)
+      if (action == "edit") {
+        if (!key) {
+          return message.channel.send("Please provide a setting to edit!")
         }
-      } else
-
-      if (action === "get") {
-        if (!key) return message.reply("Please specify a key to view");
-        if (!settings[key]) return message.reply("This key does not exist in the settings");
-        message.reply(`The value of ${key} is currently ${settings[key]}`);
-      } else {
-        message.channel.send(inspect(settings), {code: "json"});
+        if (!value) {
+          return message.channel.send("Please send a proper value for the setting!")
+        }
+        if (!modifiable[key]) {
+          return message.channel.send("That setting wasn't found!")
+        }
+        modifiable[key] = value
+        client.redisClient.set(guildId + "-SETTINGS", modifiable, function(fail, data) {
+          message.channel.send(`Successfully updated **${key}** to **${value}**!`)
+        })
       }
     }
   })
@@ -89,6 +47,6 @@ exports.conf = {
 exports.help = {
   name: "set",
   category: "System",
-  description: "View or change settings for your server.",
-  usage: "set <view/get/edit> <key> <value>\na!set edit welcomeChannel 'general'\na!set get prefix\na!set view"
+  description: "Configure server information!",
+  usage: "set [option, setting, value]\nset edit modRole Owner"
 };
