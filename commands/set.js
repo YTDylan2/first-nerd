e06cbae960374b75ad973e0c53263673abf70dd9
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-19 20:35:57 -0400

    Update set.js

--- commands/set.js
+++ commands/set.js
@@ -13,7 +13,7 @@ const redis = require('redis')
 // const action = args[0]; const key = args[1]; const value = args.slice(2);
 // OR the same as:
 // const [action, key, ...value] = args;
-exports.run = async (client, message, [action, key, ...value], level) => { // eslint-disable-line no-unused-vars
+exports.run = (client, message, [action, key, ...value], level) => { // eslint-disable-line no-unused-vars
   let guildId = message.guild.id
   client.redisClient.get(guildId + "-SETTINGS", function(err, settings) {
     if (settings) {
