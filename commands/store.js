44adadd85280a3045cc2536a10e2ec35c14ee845
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-23 15:34:53 -0400

    seticon option

--- commands/store.js
+++ commands/store.js
@@ -34,6 +34,8 @@ function shopToEmbed(shop, channel, client) {
 
 exports.run = (client, message, args, level) => {
     let guild = message.guild
+    let guildKey = guild.id + '-SHOPTEST'
+    let playerCoins = message.author.id + '-coins'
     let def = {
         'items': {
             'testitem' : {
@@ -51,7 +53,7 @@ exports.run = (client, message, args, level) => {
             icon : 'https://cdn.discordapp.com/attachments/414573970374000640/493501939816988673/vanessa_shop_icon.png'
         }
     }
-    client.redisClient.get(guild.id + '-SHOPTEST', function(err, response) {
+    client.redisClient.get(guildKey, function(err, response) {
         if (response) {
             let shopData = JSON.parse(response)
             let action = args[0]
@@ -61,14 +63,28 @@ exports.run = (client, message, args, level) => {
             }
             if (action == 'reset') {
                 if (level >= 4) {
-                    client.redisClient.set(guild.id + '-SHOPTEST', JSON.stringify(def))
+                    client.redisClient.set(guildKey, JSON.stringify(def))
                     message.channel.send("Shop successfully reset!")
                 }
                 return
             }
+            if (action == 'seticon') {
+                if (level >= 4) {
+                    let pictures = message.attachments.array()
+                    if (pictures.length == 0) {
+                        return message.channel.send("Please upload an icon to the message!")
+                    } else {
+                        let picture = pictures[0]
+                        shopData.settings.icon = picture.url
+                        client.redisClient.set(guildKey, JSON.stringify(shopData), function(err, response) {
+                            message.channel.send("Icon was successfully updated to", {files: [picture.url]})
+                        })
+                    }
+                }
+            }
         } else {
             
-            client.redisClient.set(guild.id + '-SHOPTEST', JSON.stringify(def), function(err, response) {              
+            client.redisClient.set(guildKey, JSON.stringify(def), function(err, response) {              
                 message.channel.send("Hold on, I've just set the basic settings for your server shop!")                
             })
         }
