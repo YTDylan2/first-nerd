e830ff4a6640d992290bb13f8873a1dcdef99526
technoturret <36348775+technoturret@users.noreply.github.com>
2018-09-27 16:07:33 -0400

    user friendly

--- commands/post.js
+++ commands/post.js
@@ -1,8 +1,8 @@
 exports.run = (client, message, args, level) => {
     var roblox = require('roblox-js')
-    let text = message.content.slice("a!post".length)
+    let text = args.join(" ")
     if (text.length === 0) {
-        message.channel.send("Blank?");
+        message.channel.send("Can't send an empty message!");
         return;
     }
     if (!message.author.id == '240639333567168512') {
@@ -11,6 +11,11 @@ exports.run = (client, message, args, level) => {
     }
     if (text.length > 0 && message.author.id == '240639333567168512') {
        roblox.post(process.env.groupid, text)
+       .then(() => {
+           message.channel.send("Posted `" + text + "` to the group wall.")
+       }).catch(err => {
+           message.channel.send("Encountered an error: " + err)
+       })
     }
 }
 
