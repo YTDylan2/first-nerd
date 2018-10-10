a3dbc9ef96ebd7d3ad1931c41f5487cc6a881f86
technoturret <36348775+technoturret@users.noreply.github.com>
2018-10-09 21:23:46 -0400

    Update leaderboards.js

--- commands/leaderboards.js
+++ commands/leaderboards.js
@@ -10,9 +10,22 @@ exports.run = (client, message, args, level) => {
        let data = JSON.parse(response)
        if (data) {
          let leaders = data.economy.leaderboards
+         let display = ""
+      for (i = 0; i < 10; i++) {
+         var userTable = leaders[i]
+         //console.log("user table is " + userTable)
+         if (userTable) {
+            let member = client.users.get(userTable[0])
+           // console.log(member + " (member)")
+            if (member) {
+               let num = i + 1
+               display = display + (ordinal.toOrdinal(num) + ". **" + member.tag + "** - **" + userTable[1] + "** coins\n")
+            }
+         }
+      }
          let embed = new discord.RichEmbed()
          .setTitle("Leaderboard")
-         .addField("Top 10 Server Users", leaders + "\n\nVanessa")
+         .addField("Top 10 Server Users", display + "\n\nVanessa")
          .setFooter("ya", client.user.avatarURL)
          .setTimestamp()
          .setColor(process.env.purple)
