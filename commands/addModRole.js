809f8605f13177736d5823d5b85857ade2cb4f74
technoturret <36348775+technoturret@users.noreply.github.com>
2018-10-10 21:14:09 -0400

    Update addModRole.js

--- commands/addModRole.js
+++ commands/addModRole.js
@@ -11,14 +11,14 @@ exports.run = (client, message, args, level) => {
       let data = JSON.parse(response)
       if (data) {
         let modRoles = data.data.modRoles
-        if (!modRoles[roleMention.id]) {
+        if (roleMention && !modRoles[roleMention.id]) {
           modRoles[roleMention.id] = true
           message.channel.send("Users in the role `" + roleMention.name + "` now have moderator permissions!")
           client.saveGuildData(guild, JSON.stringify(data))
         } else {
           message.channel.send("This role is already a moderator role!")
         }
-        if (!modRoles[userMention.id]) {
+        if (userMention && !modRoles[userMention.id]) {
           modRoles[userMention.id] = true
           message.channel.send(`${userMention.user.tag} is now a server moderator!`)
           client.saveGuildData(guild, JSON.stringify(data))
