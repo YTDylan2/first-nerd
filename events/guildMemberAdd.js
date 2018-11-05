51ad656c664bff429ad6d0c6ae70d885fe5d6dce
technoturret <36348775+technoturret@users.noreply.github.com>
2018-11-05 08:52:18 -0500

    upd8ed

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,13 +1,13 @@
 module.exports = (client, member) => {
   // Load the guild's settings
-  client.getGuildData(member.guild).then(settings => {
-    if (settings) {
-      settings = JSON.parse(settings)
+  client.getGuildData(member.guild).then(reply => {
+    if (reply) {
+      let settings = JSON.parse(settings)
       if (settings.welcomeEnabled !== "true") return;
 
       // replace the {user} in the welcome message with actual data
       const welcomeMessage = settings.welcomeMessage.replace("{user}", member.user.username)
-      member.guild.channels.find("name", settings.welcomeChannel).send(welcomeMessage).catch(console.log)
+      member.guild.channels.find(c => c.name == settings.welcomeChannel).send(welcomeMessage).catch(console.log)
     }
   })
 };
