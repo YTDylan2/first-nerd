40bca553f0617a1f1e8e2a23db434579f7c70a01
technoturret <36348775+technoturret@users.noreply.github.com>
2018-11-05 06:43:05 -0500

    updated
    on my phone so eghh

--- commands/anagrams.js
+++ commands/anagrams.js
@@ -49,7 +49,9 @@ exports.run = async (client, message, args, level) => {
     client.hastebin(combos)
     .then(link =>{
       message.channel.send("Anagrams posted at " + link)
-    }).catch(message.channel.send("There was an error trying to upload it to Hastebin."))
+     }).catch(e => {
+          message.channel.send("There was an error trying to upload it to Hastebin.")
+        })
 }
 
 exports.conf = {
