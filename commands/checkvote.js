e73c5c3c5bfbccbe0fa2daf8e7774061df8a178a
technoturret <36348775+technoturret@users.noreply.github.com>
2018-11-06 11:20:53 -0500

    logic mistake

--- commands/checkvote.js
+++ commands/checkvote.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args, level) => {
     let voted = await client.botlistclient.hasVoted(message.author.id)
-    if (voted) {
+    if (!voted) {
       return message.channel.send("You haven't voted! If you want to vote, run the `>support` command\nIf you just recently voted, please wait up to 3 minutes for it to register.\nVotes expire after 12 hours, so make sure to check!")
     } else {
       return message.channel.send("You've voted me up! Please remember all votes last for 12 hours.")
